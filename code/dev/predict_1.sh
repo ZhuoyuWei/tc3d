edff262b12107d1d0696505902d6d28ae5a7b792
zhuwe <zhuwe@microsoft.com>
2020-02-11 10:50:17 +0800

    add only predict sh

--- code/dev/predict_1.sh
+++ code/dev/predict_1.sh
@@ -11,7 +11,7 @@ mkdir $PRED_OUT
 mkdir /data/zhuoyu/3d/output/model/${k}
 MODEL_FILE=/data/zhuoyu/3d/output/model/${k}/model.bin
 
-python model.py train ${TRAIN_DATA} ${TRAIN_GT} ${MODEL_FILE}
+#python model.py train ${TRAIN_DATA} ${TRAIN_GT} ${MODEL_FILE}
 python model.py predict-all ${MODEL_FILE} ${PRED_DATA} ${PRED_OUT}
 python score.py score-all  ${PRED_OUT} ${PRED_ANS}
 
