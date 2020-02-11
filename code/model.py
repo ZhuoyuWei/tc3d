5739d7fa1cf871af49e6c97fa9f2c4bdb91f810c
zhuwe <zhuwe@microsoft.com>
2020-02-11 16:15:43 +0800

    MLP regression 2

--- code/model.py
+++ code/model.py
@@ -9,7 +9,7 @@ import pandas as pd
 import time
 
 from sklearn.linear_model import LinearRegression
-from sklearn.neural_networ import MLPRegressor
+from sklearn.neural_network import MLPRegressor
 
 
 @click.group()
