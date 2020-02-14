02e3ac96585748fa77b82cc8543a83859adfbb2c
zhuwe <zhuwe@microsoft.com>
2020-02-14 17:21:37 +0800

    add n_job | release boost | add sample training data 2

--- code/dlts.sh
+++ code/dlts.sh
@@ -6,6 +6,7 @@ NEstimators=$2
 MaxDepth=$3
 TreeMethod=$4
 n_job=$5
+sample_rate=$6
 
 WDATA_DIR=/data/zhuoyu/tc3d/wdata
 OUTPUT_DIR=$WDATA_DIR/$EXP_ID
@@ -50,6 +51,6 @@ mkdir $PRED_OUT_MODEL
 MODEL_FILE=$PRED_OUT_MODEL/model.bin
 
 cd $EXP_ROOT_DIR/code
-python model.py train ${TRAIN_DATA} ${TRAIN_GT} ${MODEL_FILE} ${NEstimators} ${MaxDepth} ${TreeMethod} ${n_job} 1.1 >> $OUTPUT_DIR/log
+python model.py train ${TRAIN_DATA} ${TRAIN_GT} ${MODEL_FILE} ${NEstimators} ${MaxDepth} ${TreeMethod} ${n_job} ${sample_rate} >> $OUTPUT_DIR/log
 python model.py predict-all ${MODEL_FILE} ${PRED_DATA} ${PRED_OUT} >> $OUTPUT_DIR/log
 python score.py score-all  ${PRED_OUT} ${PRED_ANS} >> $OUTPUT_DIR/log
